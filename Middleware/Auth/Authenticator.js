f02ecd1d49e3edee6e7d8426adcb35e690348c74
Cosmos <Alfredchrisayo@gmail.com>
2024-06-15 08:38:07 +0000

    add

--- Middleware/Auth/Authenticator.js
+++ Middleware/Auth/Authenticator.js
@@ -1,23 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const Auth = (req, res, next) => {
-  const token = req.cookies.userToken;
   let userToken_ = req.params.userToken;
-  if (!token && !userToken_)
+  if (!userToken_)
     res.status(401).send({ response: "sign in to your account" });
-  const userData = jwt.verify(token ? token : userToken_, process.env.api_key);
+  const userData = jwt.verify(userToken_, process.env.api_key);
   req.user = userData;
   next();
 };
 
 const TwoFa = (req, res, next) => {
   const token_ = req.params.token;
-  const token = req.cookies.Two_Fa;
-  console.log(token_);
-  if (!token && !token_) {
+  if (!token_) {
     res.status(401).send({ response: "token not found, try again" });
   }
-  const tokenData = jwt.verify(token ? token : token_, process.env.api_secret);
+  const tokenData = jwt.verify(token_, process.env.api_secret);
   req.user = tokenData;
   next();
 };
