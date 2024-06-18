4a590b993e37643172afe477b52f8f4875a02864
Cosmos <Alfredchrisayo@gmail.com>
2024-06-18 09:30:23 +0000

    add

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const cors = require("cors");
 app.use(
   cors({
-    origin: " http://localhost:5173",
+    origin: "http://localhost:5173",
     optionsSuccessStatus: 200,
     credentials: true,
   })
